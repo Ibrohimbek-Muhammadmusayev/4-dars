2b132c48827b6f5aaf6350fd7541419c1aa379a3
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2024-03-30 22:40:08 +0500

    yangi cardlar qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { apis } from './companent/api'
+import axios from 'axios'
 
 function App() {
   const [data, setData] = useState([])
   const appdata = ()=> {
     apis.get()
     .then((res) =>{
-      console.log(res.data)
       setData(res.data)
     })
   }
+
 
   useEffect(()=> {
     appdata()
   }, [])
 
-  console.log(data[1].title);
   return (
     <>
       <div className="container">
         <div className="cart-wrapper">
-          {data?.map((item) => {
-            console.log(item.title);
-            <div className="card">
-              <h1>salom</h1>
+          {data?.map((item) => (
+            <div key={item.id} className="card">
+              {console.log(item)}
+              <img className='cart-images' src={item.images} alt="images" />
+              <h1>{item.description}</h1>
             </div>
-          })}
-          <h1></h1>
+          ))}
         </div>
       </div>
     </>
